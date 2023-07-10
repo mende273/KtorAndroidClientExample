2eab6fbb1c0e9172b0f0fffb2cd97d57bed2b754
mendejumrukovski <mende@code.livelike.com>
2023-07-10 18:56:29 +0200

    create ktor http client

---
+++ app/src/main/java/mende273/ktorandroidclient/network/HttpClient.kt
@@ -0,0 +1,67 @@
+package mende273.ktorandroidclient.network
+
+import android.util.Log
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.features.DefaultRequest
+import io.ktor.client.features.defaultRequest
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.features.observer.ResponseObserver
+import io.ktor.client.request.accept
+import io.ktor.client.request.header
+import io.ktor.client.request.parameter
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.contentType
+import kotlinx.serialization.json.Json
+
+private const val TIME_OUT = 6000
+
+val httpClientAndroid = HttpClient(Android) {
+
+    install(JsonFeature) {
+
+        serializer = KotlinxSerializer(
+            Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            }
+        )
+
+        engine {
+            connectTimeout = TIME_OUT
+            socketTimeout = TIME_OUT
+        }
+    }
+
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) {
+                Log.v("Logger Ktor =>", message)
+            }
+        }
+        level = LogLevel.ALL
+    }
+
+    install(ResponseObserver) {
+        onResponse { response ->
+            Log.d("HTTP status:", "${response.status.value}")
+        }
+    }
+
+    install(DefaultRequest) {
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+    }
+
+    defaultRequest {
+        parameter("key", "value")
+        if (method != HttpMethod.Get) contentType(ContentType.Application.Json)
+        accept(ContentType.Application.Json)
+    }
+}
