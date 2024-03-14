9acd5c35799572bb417c7113d50ff58aefa68558
mende273 <mende_273@hotmail.com>
2024-03-14 01:33:21 +0100

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.5"
+        kotlinCompilerExtensionVersion = "1.5.10"
     }
 
     packaging {
@@ -58,18 +58,18 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     platform("org.jetbrains.kotlin:kotlin-bom:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    platform("androidx.compose:compose-bom:2023.10.01")
+    platform("androidx.compose:compose-bom:2024.02.02")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material3:material3:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.3")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // ktor
@@ -81,8 +81,8 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.6")
 
     // lifecycle
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
     // Koin for Android
     implementation("io.insert-koin:koin-androidx-compose:3.5.0")
