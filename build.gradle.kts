215512fc5ed3623517fb793c4bdbd7eefb296c05
mende273 <mende_273@hotmail.com>
2024-05-19 16:02:32 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     id ("com.android.application") version "8.4.0" apply false
     id ("com.android.library") version "8.4.0" apply false
-    id ("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
+    id ("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
 }
