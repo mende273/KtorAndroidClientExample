30dbe74594aa0def86200c66626a63196799e6ff
mende273 <mende_273@hotmail.com>
2024-07-28 14:47:04 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,8 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.9.1")
     platform("androidx.compose:compose-bom:2024.06.00")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -83,9 +82,9 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json")
 
     // lifecycle
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
 
     // Koin for Android
     implementation("io.insert-koin:koin-androidx-compose:3.5.6")
