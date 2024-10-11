aad0b0c31b2ced4fa90ff717722db57a00666304
mende273 <mende_273@hotmail.com>
2024-10-11 15:35:45 +0200

    update koin deprecated viewmodel package

--- app/src/main/java/mende273/ktorandroidclient/di/ViewModelModule.kt
+++ app/src/main/java/mende273/ktorandroidclient/di/ViewModelModule.kt
@@ -1,7 +1,7 @@
 package mende273.ktorandroidclient.di
 
 import mende273.ktorandroidclient.ui.screen.MainViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
