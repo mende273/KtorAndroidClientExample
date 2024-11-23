a37bc45531b706bcd6a221a8233454b388f4b17e
mende273 <mende_273@hotmail.com>
2024-11-23 11:13:11 +0100

    update compose bom to 2024.11.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.9.3")
-    platform("androidx.compose:compose-bom:2024.10.01")
+    platform("androidx.compose:compose-bom:2024.11.00")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
