8f89b0be28387ccb02069d539e3a8659b809a964
mende273 <mende_273@hotmail.com>
2025-02-20 18:52:07 +0100

    update kotlin to version 2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     id ("com.android.application") version "8.8.1" apply false
     id ("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
 }
