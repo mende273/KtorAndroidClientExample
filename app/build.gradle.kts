c4f7ca565e0dbe7a73dc48a83036d7f384f2bb2a
mende273 <mende_273@hotmail.com>
2025-05-02 01:21:41 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-compose:1.10.1")
-    platform("androidx.compose:compose-bom:2025.04.00")
+    platform("androidx.compose:compose-bom:2025.04.01")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -61,8 +61,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.8")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.8.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // coroutines
