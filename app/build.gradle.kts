42bbfb712bf1fe652e4d1a02a5970ada96c7ba6c
mende273 <mende_273@hotmail.com>
2025-11-08 01:06:34 +0100

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,24 +52,24 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.activity:activity-compose:1.11.0")
     platform("androidx.compose:compose-bom:2025.05.00")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.3.2")
+    implementation("androidx.compose.material3:material3:1.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.4")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.4")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 
     // ktor
-    implementation(platform("io.ktor:ktor-bom:3.2.3"))
+    implementation(platform("io.ktor:ktor-bom:3.3.2"))
     implementation("io.ktor:ktor-client-android")
     implementation("io.ktor:ktor-client-serialization")
     implementation("io.ktor:ktor-client-logging")
@@ -77,9 +77,9 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json")
 
     // lifecycle
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
 
     // Koin for Android
     implementation("io.insert-koin:koin-androidx-compose:4.1.1")
