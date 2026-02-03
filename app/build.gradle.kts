1a75202bc2061f78d3518bff0acf1dc1f20885a2
mende273 <mende_273@hotmail.com>
2026-02-03 01:48:44 +0100

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.activity:activity-compose:1.12.2")
+    implementation("androidx.activity:activity-compose:1.12.3")
     platform("androidx.compose:compose-bom:2025.05.00")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -61,15 +61,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.10.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.10.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.10.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.10.2")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 
     // ktor
-    implementation(platform("io.ktor:ktor-bom:3.3.3"))
+    implementation(platform("io.ktor:ktor-bom:3.4.0"))
     implementation("io.ktor:ktor-client-android")
     implementation("io.ktor:ktor-client-serialization")
     implementation("io.ktor:ktor-client-logging")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id ("com.android.application") version "8.13.2" apply false
-    id ("org.jetbrains.kotlin.android") version "2.2.21" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+    id ("com.android.application") version "9.0.0" apply false
+    id ("org.jetbrains.kotlin.android") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
 }
