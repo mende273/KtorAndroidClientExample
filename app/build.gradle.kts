e2793065e504a79f0eb12fd4056737a9eb90b126
mende273 <mende_273@hotmail.com>
2026-03-06 01:24:49 +0100

    clean up deprecations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("kotlinx-serialization")
     id("org.jetbrains.kotlin.plugin.compose")
 }
